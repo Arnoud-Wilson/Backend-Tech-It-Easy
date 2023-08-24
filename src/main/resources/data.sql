70291cfa9185b4ac7a89bff9952747395eec5121
Arnoud-Wilson <arnoud608@gmail.com>
2023-08-24 16:07:51 +0200

    added application.properties and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO books (id, isbn, title, genre)
+VALUES
+(1, ‘1232143143’, ‘Novi’, ‘educatief’),
+(2,’563455423412’,’Koken met bananen’,’kookboeken’);
+
+//TODO: aanpassen naar television
