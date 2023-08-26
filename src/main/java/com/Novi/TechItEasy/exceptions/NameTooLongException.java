52eb2a50c8e5f93f507aa6612ce643bcbcabf342
Arnoud-Wilson <arnoud608@gmail.com>
2023-08-26 08:22:33 +0200

    modified application.properties

--- src/main/java/com/Novi/TechItEasy/exceptions/NameTooLongException.java
+++ src/main/java/com/Novi/TechItEasy/exceptions/NameTooLongException.java
@@ -1,20 +1,15 @@
 package com.Novi.TechItEasy.exceptions;
 
 
-// Deze klasse vormt onze custom exception
 public class NameTooLongException extends RuntimeException {
 
-    // De exception zonder message
     public NameTooLongException() {
 
         super();
-
     }
 
-    // De exception met message
     public NameTooLongException(String message) {
 
         super(message);
-
     }
 }
--- src/main/java/com/Novi/TechItEasy/exceptions/RecordNotFoundException.java
+++ src/main/java/com/Novi/TechItEasy/exceptions/RecordNotFoundException.java
@@ -1,20 +1,16 @@
 package com.Novi.TechItEasy.exceptions;
 
 
-// Deze klasse vormt onze custom exception
 public class RecordNotFoundException extends RuntimeException {
 
-    // De exception zonder message
+
     public RecordNotFoundException() {
 
         super();
-
     }
 
-    // De exception met message
     public RecordNotFoundException(String message) {
 
         super(message);
-
     }
 }
