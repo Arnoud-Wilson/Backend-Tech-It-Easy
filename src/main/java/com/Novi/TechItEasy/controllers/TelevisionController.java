2390c0107f921a1fdfb4f2c4308a4a9a58696858
Arnoud-Wilson <arnoud608@gmail.com>
2023-08-28 20:04:25 +0200

    modified GetMapping methods

--- src/main/java/com/Novi/TechItEasy/controllers/TelevisionController.java
+++ src/main/java/com/Novi/TechItEasy/controllers/TelevisionController.java
@@ -1,12 +1,14 @@
 package com.Novi.TechItEasy.controllers;
 
 import com.Novi.TechItEasy.exceptions.NameTooLongException;
+import com.Novi.TechItEasy.models.Television;
 import com.Novi.TechItEasy.repositories.TelevisionRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -19,43 +21,40 @@ public class TelevisionController {
     }
 
 
-    String[] televisionBrands = {"Sony", "Phillips", "LG"};
-    List<String> televisionDatabase = new ArrayList<>(List.of(televisionBrands));
-
-
     @GetMapping(value ="/televisions")
-    public ResponseEntity<List<String>> getTelevisions() {
-        return ResponseEntity.ok(televisionDatabase);
+    public ResponseEntity<List<Television>> getTelevisions() {
+
+        return ResponseEntity.ok(televisionRepository.findAll());
     }
 
     @GetMapping(value ="/television/{id}")
-    public ResponseEntity<String> getTelevision(@PathVariable int id) {
-        if (id >= televisionDatabase.size()) {
+    public ResponseEntity<Optional<Television>> getTelevision(@PathVariable Long id) {
+        if (televisionRepository.existsById(id)) {
+            return ResponseEntity.ok(televisionRepository.findById(id));
+        } else{
             throw new IndexOutOfBoundsException();
-        } else{
-            return ResponseEntity.ok(televisionDatabase.get(id));
         }
     }
 
-    @PostMapping(value ="/television/add")
-    private ResponseEntity<List<String>> ChangeTelevision(@RequestBody String name) {
-        if (name.length() >= 20) {
-            throw new NameTooLongException();
-        } else{
-            televisionDatabase.add(name);
-            return ResponseEntity.ok(televisionDatabase);
-        }
-    }
-
-    @PutMapping(value ="/television/change/{id}")
-    public ResponseEntity<List<String>> addTelevision(@PathVariable int id, @RequestBody String name) {
-        televisionDatabase.set(id, name);
-        return ResponseEntity.ok(televisionDatabase);
-    }
-
-    @DeleteMapping(value ="/television/delete/{id}")
-    public ResponseEntity<List<String>> deleteTelevision(@PathVariable int id) {
-        televisionDatabase.remove(id);
-        return ResponseEntity.ok(televisionDatabase);
-    }
+//    @PostMapping(value ="/television/add")
+//    private ResponseEntity<List<String>> ChangeTelevision(@RequestBody String name) {
+//        if (name.length() >= 20) {
+//            throw new NameTooLongException();
+//        } else{
+//            televisionDatabase.add(name);
+//            return ResponseEntity.ok(televisionDatabase);
+//        }
+//    }
+//
+//    @PutMapping(value ="/television/change/{id}")
+//    public ResponseEntity<List<String>> addTelevision(@PathVariable int id, @RequestBody String name) {
+//        televisionDatabase.set(id, name);
+//        return ResponseEntity.ok(televisionDatabase);
+//    }
+//
+//    @DeleteMapping(value ="/television/delete/{id}")
+//    public ResponseEntity<List<String>> deleteTelevision(@PathVariable int id) {
+//        televisionDatabase.remove(id);
+//        return ResponseEntity.ok(televisionDatabase);
+//    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO television(ambi_light, available_size, bluetooth, original_stock, price, refresh_rate, smart_tv, sold, voice_control, wifi, brand, name, screen_quality, screen_type, type)
+VALUES
+    (true, 42, true, 5, 500, 100, true, 3, false, true, 'test', 'testéén', 'good', 'led', 'led'),
+    (true, 42, true, 5, 400, 100, true, 2, false, true, 'test', 'testtwee', 'good', 'led', 'led');
