79c810c5f75e16b67582b2778300729e1210ae0f
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-02 09:50:41 +0200

    added TelevisionDto

---
+++ src/main/java/com/Novi/TechItEasy/dtos/TelevisionDto.java
@@ -0,0 +1,51 @@
+package com.Novi.TechItEasy.dtos;
+
+
+import com.Novi.TechItEasy.models.Television;
+
+public class TelevisionDto {
+
+    private Long id;
+    private String brand;
+    private String name;
+    private String type;
+    private Double price;
+    private Double availableSize;
+    private Double refreshRate;
+    private String screenType;
+    private String screenQuality;
+    private Boolean smartTv;
+    private Boolean wifi;
+    private Boolean voiceControl;
+    private Boolean hdr;
+    private Boolean bluetooth;
+    private Boolean ambiLight;
+    private int originalStock;
+    private int sold;
+
+    public static TelevisionDto fromTelevision(Television television) {
+
+        TelevisionDto dto = new TelevisionDto();
+
+        dto.id = television.getId();
+        dto.brand = television.getBrand();
+        dto.name = television.getName();
+        dto.type = television.getType();
+        dto.price = television.getPrice();
+        dto.availableSize = television.getAvailableSize();
+        dto.refreshRate = television.getRefreshRate();
+        dto.screenType = television.getScreenType();
+        dto.screenQuality = television.getScreenQuality();
+        dto.smartTv = television.getSmartTv();
+        dto.wifi = television.getWifi();
+        dto.voiceControl = television.getVoiceControl();
+        dto.hdr = television.getHdr();
+        dto.bluetooth = television.getBluetooth();
+        dto.ambiLight = television.getAmbiLight();
+        dto.originalStock = television.getOriginalStock();
+        dto.sold = television.getSold();
+
+        return dto;
+    }
+
+}
--- src/main/java/com/Novi/TechItEasy/services/TelevisionService.java
+++ src/main/java/com/Novi/TechItEasy/services/TelevisionService.java
@@ -1,9 +1,12 @@
 package com.Novi.TechItEasy.services;
 
+import com.Novi.TechItEasy.models.Television;
 import com.Novi.TechItEasy.repositories.TelevisionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 
 @Service
@@ -11,6 +14,11 @@ public class TelevisionService {
 
     @Autowired
     private TelevisionRepository televisionRepository;
+
+    public List<Television> getTelevisions() {
+
+        return televisionRepository.findAll();
+    }
 
 
 }
