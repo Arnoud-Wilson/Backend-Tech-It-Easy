f754a04cc81f6fbdaa4ee5382673176c822b196e
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-18 20:40:11 +0200

    made repositories

---
+++ src/main/java/com/Novi/TechItEasy/repositories/CI_ModuleRepository.java
@@ -0,0 +1,8 @@
+package com.Novi.TechItEasy.repositories;
+
+import com.Novi.TechItEasy.models.CI_Module;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CI_ModuleRepository extends JpaRepository<CI_Module, Long> {
+
+}
