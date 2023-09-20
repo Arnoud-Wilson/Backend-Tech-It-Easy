bcac55f5ac4a442a10a2f4d7e129da7a267bdc14
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-20 21:43:38 +0200

    made get all ci modules controller/service

--- src/main/java/com/Novi/TechItEasy/controllers/CI_ModuleController.java
+++ src/main/java/com/Novi/TechItEasy/controllers/CI_ModuleController.java
@@ -1,4 +1,71 @@
 package com.Novi.TechItEasy.controllers;
 
+import com.Novi.TechItEasy.dtos.CI_ModuleDto;
+import com.Novi.TechItEasy.dtos.WallBracketDto;
+import com.Novi.TechItEasy.services.CI_ModuleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/ci_modules")
 public class CI_ModuleController {
+
+    private final CI_ModuleService ci_moduleService;
+
+    public CI_ModuleController(CI_ModuleService ci_moduleService) {
+        this.ci_moduleService = ci_moduleService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<CI_ModuleDto>> getCiModules() {
+
+        return ResponseEntity.ok(ci_moduleService.getCiModules());
+    }
+
+//
+//    @GetMapping(value ="/{id}")
+//    public ResponseEntity<WallBracketDto> getWallBracket(@PathVariable Long id) {
+//
+//        return ResponseEntity.ok(wallBracketService.getWallBracket(id));
+//    }
+//
+//
+//    @PostMapping
+//    public ResponseEntity<Object> createWallBracket(@Valid @RequestBody WallBracketInputDto wallBracket, BindingResult bindingResult) {
+//
+//        if (bindingResult.hasFieldErrors()) {
+//            StringBuilder stringBuilder = new StringBuilder();
+//            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+//                stringBuilder.append(fieldError.getField());
+//                stringBuilder.append(": ");
+//                stringBuilder.append(fieldError.getDefaultMessage());
+//                stringBuilder.append("\n");
+//            }
+//            return ResponseEntity.badRequest().body(stringBuilder);
+//        } else {
+//
+//            WallBracketDto dto = wallBracketService.createWallBracket(wallBracket);
+//            URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().path("/" + dto.id).toUriString());
+//
+//            return ResponseEntity.created(uri).body(dto);
+//        }
+//    }
+//
+//
+//    @PutMapping(value ="/{id}")
+//    public ResponseEntity<WallBracketDto> changeWallBracket(@PathVariable Long id, @RequestBody WallBracketDto wallBracket) {
+//
+//        return ResponseEntity.ok(wallBracketService.changeWallBracket(id, wallBracket));
+//    }
+//
+//
+//    @DeleteMapping(value ="/{id}")
+//    public ResponseEntity<String> deleteWallBracket(@PathVariable Long id) {
+//
+//        return ResponseEntity.ok(wallBracketService.deleteWallBracket(id));
+//    }
 }
--- src/main/java/com/Novi/TechItEasy/services/CI_ModuleService.java
+++ src/main/java/com/Novi/TechItEasy/services/CI_ModuleService.java
@@ -1,4 +1,118 @@
 package com.Novi.TechItEasy.services;
 
+import com.Novi.TechItEasy.dtos.CI_ModuleDto;
+import com.Novi.TechItEasy.dtos.WallBracketDto;
+import com.Novi.TechItEasy.exceptions.RecordNotFoundException;
+import com.Novi.TechItEasy.helpers.DtoConverters;
+import com.Novi.TechItEasy.models.CI_Module;
+import com.Novi.TechItEasy.models.WallBracket;
+import com.Novi.TechItEasy.repositories.CI_ModuleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CI_ModuleService {
+
+    private final CI_ModuleRepository ci_moduleRepository;
+
+    public CI_ModuleService(CI_ModuleRepository ci_moduleRepository) {
+        this.ci_moduleRepository = ci_moduleRepository;
+    }
+
+
+    /// For fetching all CI modules currently in the database /////
+    public List<CI_ModuleDto> getCiModules() {
+
+        List<CI_Module> CI_Modules = new ArrayList<>(ci_moduleRepository.findAll());
+        List<CI_ModuleDto> CI_ModuleDtos = new ArrayList<>();
+
+        for (CI_Module ci_module : CI_Modules) {
+            CI_ModuleDto dto = new CI_ModuleDto();
+            DtoConverters.CI_ModuleDtoConverter(ci_module, dto);
+            CI_ModuleDtos.add(dto);
+        }
+
+        if (CI_ModuleDtos.isEmpty()) {
+            throw new RecordNotFoundException("We hebben geen CI modules om te laten zien");
+        } else {
+            return CI_ModuleDtos;
+        }
+    }
+
+//
+//    /// For fetching one wall bracket by id /////
+//    public WallBracketDto getWallBracket(Long id) {
+//        Optional<WallBracket> fetchedWallBracket = wallBracketRepository.findById(id);
+//
+//        if (fetchedWallBracket.isPresent()) {
+//            WallBracketDto dto = new WallBracketDto();
+//            DtoConverters.wallBracketDtoConverter(fetchedWallBracket.get(), dto);
+//            return dto;
+//
+//        } else {
+//            throw new RecordNotFoundException("We hebben geen muurbeugel met dit ID.");
+//        }
+//    }
+//
+//
+//    /// For creating one new wall bracket in database /////
+//    public WallBracketDto createWallBracket(WallBracketInputDto wallBracket) {
+//
+//        WallBracket newWallBracket = new WallBracket();
+//        DtoConverters.wallBracketInputDtoConverter(newWallBracket, wallBracket);
+//
+//        wallBracketRepository.save(newWallBracket);
+//
+//        WallBracketDto dto = new WallBracketDto();
+//        DtoConverters.wallBracketDtoConverter(newWallBracket, dto);
+//
+//
+//        return dto;
+//    }
+//
+//
+//    ///// For changing wall bracket in database /////
+//    public WallBracketDto changeWallBracket(Long id, WallBracketDto wallBracketDto) {
+//
+//        Optional<WallBracket> databaseWallBracket = wallBracketRepository.findById(id);
+//
+//        if (databaseWallBracket.isPresent()) {
+//            WallBracket fetchedWallBracket = databaseWallBracket.get();
+//
+//            if (wallBracketDto.size != null) {
+//                fetchedWallBracket.setSize(wallBracketDto.size);
+//            }
+//            if (wallBracketDto.adjustable != null) {
+//                fetchedWallBracket.setAdjustable(wallBracketDto.adjustable);
+//            }
+//            if (wallBracketDto.name != null) {
+//                fetchedWallBracket.setName(wallBracketDto.name);
+//            }
+//            if (wallBracketDto.price != null) {
+//                fetchedWallBracket.setPrice(wallBracketDto.price);
+//            }
+//
+//            wallBracketRepository.save(fetchedWallBracket);
+//            DtoConverters.wallBracketDtoConverter(wallBracketRepository.findById(id).get(), wallBracketDto);
+//
+//            return wallBracketDto;
+//
+//        } else {
+//            throw new RecordNotFoundException("We hebben geen muurbeugels met dit ID.");
+//        }
+//    }
+//
+//
+//    ///// For deleting wall bracket from database /////
+//    public String deleteWallBracket(Long id) {
+//        if (wallBracketRepository.existsById(id)) {
+//            wallBracketRepository.deleteAllById(Collections.singleton(id));
+//
+//            return "We hebben muurbeugel met id: " + id + " uit de database verwijderd.";
+//        } else {
+//            throw new RecordNotFoundException("We hebben geen muurbeugel met dit ID.");
+//        }
+//    }
 }
