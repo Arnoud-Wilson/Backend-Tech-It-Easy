246b90bcd7b441af382e527424c2f19abd07227a
Arnoud-Wilson <arnoud608@gmail.com>
2023-10-12 11:06:23 +0200

    added exception handlers

--- src/main/java/com/Novi/TechItEasy/controllers/exceptions/ExceptionController.java
+++ src/main/java/com/Novi/TechItEasy/controllers/exceptions/ExceptionController.java
@@ -1,7 +1,9 @@
 package com.Novi.TechItEasy.controllers.exceptions;
 
+import com.Novi.TechItEasy.exceptions.BadRequestException;
 import com.Novi.TechItEasy.exceptions.MinimalRequiredTelevisionException;
 import com.Novi.TechItEasy.exceptions.RecordNotFoundException;
+import com.Novi.TechItEasy.exceptions.UsernameNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -23,5 +25,15 @@ public class ExceptionController {
     @ExceptionHandler(value = MinimalRequiredTelevisionException.class)
     public ResponseEntity<String> exception(MinimalRequiredTelevisionException exception) {
         return new ResponseEntity<>("De televisie moet minimaal een merk en naam hebben", HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<String> exception(BadRequestException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = UsernameNotFoundException.class)
+    public ResponseEntity<String> exception(UsernameNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_ACCEPTABLE);
     }
 }
