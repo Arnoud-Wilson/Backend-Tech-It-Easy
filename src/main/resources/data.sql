5bc77695883676b91a877746976bba5234183fdc
Arnoud-Wilson <arnoud608@gmail.com>
2023-10-12 11:28:14 +0200

    added data.sql security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ INSERT INTO television(ambi_light, available_size, bluetooth, original_stock, pr
 VALUES
     (true, 42, true, 5, 500, 100, true, 1, false, true, 'test', 'testéén', 'good', 'led', 'led'),
     (true, 42, true, 5, 400, 100, true, 2, false, true, 'test', 'testtwee', 'good', 'led', 'led');
+
+
+INSERT INTO users (username, password, enabled, apikey, email) VALUES ('henk', /*TODO encrypted password (https://bcrypt-generator.com))*/, true, '7847493', "test@testy.tst");
+INSERT INTO authorities (username, authority) VALUES ('henk', 'ROLE_ADMIN');
